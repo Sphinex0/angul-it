44a719e09ec8224156c9f997ef5c54f6792f96ac
aelmir || Sphinex0 <adnane.elmir1@gmail.com>
2026-01-31 18:08:36 +0100

    save

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { completionGuard } from './core/guards/completion.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./features/home/home.component').then(m => m.HomeComponent),
+    title: 'Angul-It | Welcome'
+  },
+  {
+    path: 'challenge',
+    loadComponent: () => import('./features/captcha/captcha.component').then(m => m.CaptchaComponent),
+    title: 'Angul-It | Verify'
+  },
+  {
+    path: 'result',
+    loadComponent: () => import('./features/result/result.component').then(m => m.ResultComponent),
+    canActivate: [completionGuard], // 🔒 Protects the route
+    title: 'Angul-It | Success'
+  },
+  { path: '**', redirectTo: '' }
+];
